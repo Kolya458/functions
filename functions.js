ab53d7e51be603033fa53cf92479eae914322df9
Kolya458 <kononovnickolay@yahoo.com>
2019-04-10 14:03:29 +0300

    four tasks

---
+++ functions.js
@@ -0,0 +1,54 @@
+// 1
+function splitAndMerge(str, sp){
+    let a = str.split(" ");
+    let b = [];
+    for(i=0; i<a.length; i++){
+        b[i] = a[i].split("").join(sp);
+    }
+    return b.join(" ");
+}
+
+//2
+function convert(obj){
+    let a = [];
+    for (var key in obj){
+        let b = [];
+        b.push(key);
+        b.push(obj[key]);
+        a.push(b);
+    }
+    return a;
+}
+
+// 3
+function toCamelCase(string=""){
+
+    if (string.indexOf("-") > -1) {
+    const a = string.split("-");
+    for (i=0; i<a.length; i++){
+        a[i] = a[i].charAt(0).toUpperCase().concat(a[i].slice(1, a[i].length));
+        }
+    return a.join("");
+    }
+
+    if (string.indexOf("_") > -1) {
+        const a = string.split("_");
+        for (i=0; i<a.length; i++){
+            a[i] = a[i].charAt(0).toUpperCase().concat(a[i].slice(1, a[i].length));
+        }
+        return a.join("");
+    }
+}
+
+// 4
+function wordReverse(str){
+    let a = str.split(" ");
+    let b = [];
+    a.forEach( (element, i) => {
+         b[i] = element.split("").reverse().join("");
+    });
+    return b.join(" ");
+}
+
+
+
